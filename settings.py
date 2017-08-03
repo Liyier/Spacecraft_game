29046c448a53271c83c9e989e8e42d43f78ba50f
yier <863022689@qq.com>
2017-08-04 01:55:05 +0800

    storage all the settings by create a class

---
+++ settings.py
@@ -0,0 +1,14 @@
+"""
+存储该游戏所有设置的类
+"""
+
+
+class Settings(object):
+
+    def __init__(self):
+
+        # 屏幕设置
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        
